fca0bf576ac9ce63ddbc9225b8b968c5fe4c438e
江口慎悟 <eguchishingo@eguchishingonoMacBook-ea.local>
2018-06-11 23:59:30 +0900

    minor

--- test_synquid_plus.sh
+++ test_synquid_plus.sh
@@ -18,7 +18,7 @@ TEST_FILE=(list-intersection
 
 for file in ${TEST_FILE[@]}
 do
-    time "${SCRIPT_DIR}/synquid_plus.sh" $TEST_FILE_DIR$file
+    time "${SCRIPT_DIR}/synquid_plus.sh" $TEST_FILE_DIR$file "fold"
 done
 
 time "${SCRIPT_DIR}/synquid_plus.sh" "${TEST_FILE_DIR}test_sort" "merge"
