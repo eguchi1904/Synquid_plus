bbdcd96b0983176d44a6b4b465ca0cc12159e655
Eguchi <eguchi.1904@gmail.com>
2019-04-11 23:20:56 +0900

    DyState.next

--- solver.ml
+++ solver.ml
@@ -1343,15 +1343,36 @@ sig
 
   type t
      
-  (* val fix_constraint: t -> G.t -> G.pLavel -> G.cLavel -> unit *)
+         (* val fix_constraint: t -> G.t -> G.pLavel -> G.cLavel -> unit *)
+
+  val next: t -> G.t -> Formula.t M.t ->
+            (G.pLavel * (G.cLavel * Formula.qformula) list) option
 
 end = struct
    
-  type t = {fixability: FixabilityManager.t
-           ;pFixState: PFixState.t
+  type t = {fixabilityManager: FixabilityManager.t
            ;cFixState: CFixState.t
+           ;pFixableCounter: PFixableConstraintCounter.t
+           ;pFixState: PFixState.t
            ;queue: PriorityQueue.t
            }
-         (* let tell_constraint_pos_predicate_is_fixed t graph p c = *)
+             (* let tell_constraint_pos_predicate_is_fixed t graph p c = *)
+
+  let next t graph assign =
+    match PriorityQueue.pop t.queue with
+    |None -> None
+    |Some (p, pol, priority) ->
+      let sol = FixabilityManager.fix t.fixabilityManager
+                                      graph assign p priority
+                                      ~may_change:(t.cFixState,
+                                                   t.pFixableCounter,
+                                                   t.pFixState,
+                                                   t.queue)
+      in
+      Some (p, sol)
+    
+    
+         
+         
 
 end
